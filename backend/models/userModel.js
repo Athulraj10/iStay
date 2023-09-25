2da1d011bbb2b8319e5ba94e83b241a0a8ea2213
Athulraj10 <athulraj165@gmail.com>
2023-09-25 21:39:58 +0530

    user model salt keyword

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -26,6 +26,7 @@ userSchema.pre('save',async function(next){
     if(!this.isModified('password')){
         next();
     }
+//salt means hashing Number of times its inbuild in bcrypt module
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password,salt)
 });
