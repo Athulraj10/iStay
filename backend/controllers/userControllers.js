5c2d3cd7cbbcdf5238a4cb36c33c69bcf4060ccc
Athulraj10 <athulraj165@gmail.com>
2023-09-25 20:35:31 +0530

    mongodb connected using Compose

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,9 +1,44 @@
 import asyncHnadler from 'express-async-handler'
 
+//@desc Auth user/set token
+//access Public
+//route POST// /api/users
 const authUser =asyncHnadler(async (req,res)=>{
     res.status(200).json({message:"Auth user"})
 });
 
+//@desc create user
+//access Public
+//route POST// /api/register
+const registerUser =asyncHnadler(async (req,res)=>{
+    res.status(200).json({message:"Register user"})
+});
+//@desc logout USer
+//access Public
+//route POST// /api/logout
+const logoutUser =asyncHnadler(async (req,res)=>{
+    res.status(200).json({message:"Logout User"})
+});
+
+//@desc get user profile
+//access Private
+//route POST// /api/users/profile
+const getUserProfile =asyncHnadler(async (req,res)=>{
+    res.status(200).json({message:"User profile"})
+});
+
+//@desc get update user profile
+//access Private
+//route PUT// /api/users/profile
+const updateUserProfile =asyncHnadler(async (req,res)=>{
+    res.status(200).json({message:"update user profile"})
+});
+
+
 export {
-    authUser
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile
 }
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
+import userRoutes from "./routes/userRoutes.js"
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 import express from "express"
 import dotenv from 'dotenv'
 dotenv.config();
 const port = process.env.PORT || 5000
-import userRoutes from "./routes/userRoutes.js"
 
 const app =express();
 
@@ -10,4 +11,6 @@ app.use('/api/users',userRoutes)
 
 app.get('/',(req,res)=>res.send("server is ready"))
 
+app.use(notFound)
+app.use(errorHandler)
 app.listen(port,()=>console.log(`server start port ${port}`))
