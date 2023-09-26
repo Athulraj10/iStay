ce93ad4b3a2afb3bc88bac3fb47823bae829437c
Athulraj10 <athulraj165@gmail.com>
2023-09-26 19:11:03 +0530

    Created header and made a Navbar container

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const handleChange=()=>{
-    setCount(count+1)
-  }
-
   return (
     <div className='app'>
-      <h1 >{count}</h1>
-      <button onClick={handleChange}>+</button>
-    </div>
+     <Header/>
+      </div>
   )
 }
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port:3000,
-    proxy:{
+    proxy: {
       '/api':{
         target:'http://localhost:5000',
         changeOrigin:true,
