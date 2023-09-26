c8f78ecf64b0022292678e03af78fcef43e92cb1
Athulraj10 <athulraj165@gmail.com>
2023-09-26 20:36:08 +0530

    created a router component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Outlet } from 'react-router-dom'
 import Header from './components/Header'
 
-function App() {
+const  App = () => {
   return (
-    <div className='app'>
+    <>
      <Header/>
-      </div>
+      <Outlet/>
+      </>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,RouterProvider} from 'react-router-dom'
 import './index.css'
+import HomeScreen from './screen/HomeScreen/HomeScreen.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={ <App/> }>
+      <Route index={true} path='/' element={ <HomeScreen/> }/>
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
