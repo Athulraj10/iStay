e3850749b9973ddb9cf72742839601558753eb89
Athulraj10 <athulraj165@gmail.com>
2023-09-26 21:23:54 +0530

    Header working on link pages link containers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
-import { Outlet } from 'react-router-dom'
-import Header from './components/Header'
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
 
-const  App = () => {
+const App = () => {
   return (
     <>
-     <Header/>
-      <Outlet/>
-      </>
-  )
-}
+      <Header />
+      <Container className="my-2">
+        <Outlet />
+      </Container>
+    </>
+  );
+};
 
-export default App
+export default App;
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,21 +1,30 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
 
 const Header = () => {
   return (
     <header>
-      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>MERN App</Navbar.Brand>
-          <Navbar.Toggle aria-controls='basic-navbar-nav' />
-          <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='ms-auto'>
-              <Nav.Link href='/login'>
-                <FaSignInAlt /> Sign In
-              </Nav.Link>
-              <Nav.Link href='/login'>
+          <LinkContainer to="/">
+            <Navbar.Brand>MERN App</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+             
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <FaSignInAlt /> Sign In
+                </Nav.Link>
+              </LinkContainer>
+             
+             <LinkContainer>
+             <Nav.Link>
                 <FaSignOutAlt /> Sign Up
               </Nav.Link>
+             </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
