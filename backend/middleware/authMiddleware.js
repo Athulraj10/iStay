1543c113ae0f061fe68085910128ce15cba048e3
Athulraj10 <athulraj165@gmail.com>
2023-09-26 15:00:19 +0530

    in mongodb IP address change to 0.0.0.0 shows internal connection error

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -5,15 +5,17 @@ import User from "../models/userModel.js";
 const protect = asyncHandler(async (req,res,next) => {
     let token ; 
     token = req.cookies.jwt;
-    console.log(token)
+    // console.log(token)
     if(token){
         try {
             const decodedToken = jwt.verify(token,process.env.JWT_SECRET);
+            console.log(decodedToken)
             req.user = await User.FindbyId(decodedToken.userId).select('-password');
+            console.log(req.user)
             next();
         } catch (error) {
             res.status(401);
-            throw new Error('Not Authorized ,Invalid Token ')
+            throw new Error('Invalid Token ')
         }
     }else{
         res.status(401);
