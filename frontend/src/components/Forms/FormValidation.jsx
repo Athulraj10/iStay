517939f342f293367d0e51e801706b68b0f90460
username <your-email@example.com>
2023-09-27 21:27:14 +0530

    Form validation reset

--- frontend/src/components/Forms/FormValidation.jsx
+++ frontend/src/components/Forms/FormValidation.jsx
@@ -1,16 +1,2 @@
 import { useState } from "react";
 import "./FormValidation.jsx";
-
-const FormValidation = (props) => {
-
-  return (
-    <div className="formInput">
-      <label>{label}</label>
-      <input
-     
-      <span>{errorMessage}</span>
-    </div>
-  );
-};
-
-export default FormValidation;
