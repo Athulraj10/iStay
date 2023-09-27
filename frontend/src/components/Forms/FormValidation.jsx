d36f422078e285f552970a842f53a6d02684e4cd
username <your-email@example.com>
2023-09-27 21:17:40 +0530

    going to build validation in register form

--- frontend/src/components/Forms/FormValidation.jsx
+++ frontend/src/components/Forms/FormValidation.jsx
@@ -1,11 +1,29 @@
+import { useState } from "react";
+import "./FormValidation.jsx";
 
+const FormValidation = (props) => {
+  const [focused, setFocused] = useState(false);
+  const { label, errorMessage, onChange, id, ...inputProps } = props;
 
-const FormValidation = () => {
+  const handleFocus = (e) => {
+    setFocused(true);
+  };
+
   return (
-    <div className='FormInput'>
-
+    <div className="formInput">
+      <label>{label}</label>
+      <input
+        {...inputProps}
+        onChange={onChange}
+        onBlur={handleFocus}
+        onFocus={() =>
+          inputProps.name === "confirmPassword" && setFocused(true)
+        }
+        focused={focused.toString()}
+      />
+      <span>{errorMessage}</span>
     </div>
-  )
-}
+  );
+};
 
-export default FormValidation
+export default FormValidation;
