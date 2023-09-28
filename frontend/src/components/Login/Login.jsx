dbabe5b14d8496d5499389c2d72d40e1d6cc7a24
username <your-email@example.com>
2023-09-28 21:58:17 +0530

    last register

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,59 +1,60 @@
+import { Button, Form, Row, Col } from "react-bootstrap";
 import React, { useState } from "react";
-import { Button, Col, Form, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import FormContainer from "../Forms/FormContainer";
+import './Login.css'
+
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const sumbitHandler = (e) => {
+  const sumbitHandler = () => {
     e.preventDefault();
-    console.log("submitted");
+    console.log("value submitted");
   };
   return (
-    <FormContainer>
-      <h1>Login </h1>
-      {/* Normally we are useing forms */}
-      <Form onSubmit={sumbitHandler}>
-        {/* for wraping our content into a group of items like Div Started */}
-        <Form.Group className="my-2" controlId="email">
-          {/* Normal like a label input box top Nameing Email Address */}
-          <Form.Label>Email Address</Form.Label>
-          {/* Controll is LIke a input box same as a HTMl */}
-          <Form.Control
-            type="email"
-            placeholder="Enter Email"
-            value={email}
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
-          ></Form.Control>
-          {/* for wraping our content into a group Ended */}
-        </Form.Group>
+    <div className='formInput'>
+      <FormContainer>
+        <h1>Sign In </h1>
+        <Form onSubmit={sumbitHandler}>
 
-        {/* for wraping our content into a group of items like Div Started */}
-        <Form.Group className="my-2" controlId="password">
-          {/* Normal like a label input box top Nameing Email Address */}
-          <Form.Label>Email Password</Form.Label>
-          {/* Controll is LIke a input box same as a HTMl */}
-          <Form.Control
-            type="password"
-            placeholder="Enter Password"
-            value={password}
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-          ></Form.Control>
-          {/* for wraping our content into a group Ended */}
-        </Form.Group>
-
-        {/* creating a button for submit values */}
-        <Button type="submit" variant="primary" className="mt-3">
-          Register
-        </Button>
-      </Form>
-    </FormContainer>
+            {/* User Email Entering Place and Stored in State SetEmail  */}
+            <Form.Group className="my-2" controlId="email">
+            <Form.Label>Email Address</Form.Label>
+                <Form.Control
+                type="email"
+                placeholder="Enter Email"
+                value={email}
+                onChange={(e)=>setEmail(e.target.value)}
+                >
+                </Form.Control>
+            </Form.Group>
+            
+            {/* User Password Entering Place and Stored in State  */}
+            <Form.Group className="my-2" controlId="password">
+            <Form.Label>Email Address</Form.Label>
+                <Form.Control
+                type="password"
+                placeholder="Enter Password"
+                value={password}
+                onChange={(e)=>setPassword(e.target.value)}
+                >
+                </Form.Control>
+            </Form.Group>
+            <Button type="submit" variant="primary" className="mt-3">
+                Sign In
+            </Button>
+           
+            {/* If user Already registered then Directed to register page  */}
+            <Row className="py-3">
+                <Col>
+                New Customer ? <Link to='/register'>Register</Link>
+                </Col>
+            </Row>
+        </Form>
+      </FormContainer>
+    </div>
   );
 };
 
