7730b2e1a1876c00831da5b62a53d84010c4c8f6
username <your-email@example.com>
2023-09-28 17:47:15 +0530

    login completed moving to Register page

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,60 +1,62 @@
-import { Button, Form, Row, Col } from "react-bootstrap";
 import React, { useState } from "react";
+import { Button, Col, Form, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import FormContainer from "../Forms/FormContainer";
-import './Login.css'
-
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const sumbitHandler = () => {
+  const sumbitHandler = (e) => {
     e.preventDefault();
-    console.log("value submitted");
+    console.log("submitted");
   };
   return (
-    <div className='formInput'>
-      <FormContainer>
-        <h1>Sign In </h1>
-        <Form onSubmit={sumbitHandler}>
+    <FormContainer>
+      <h1>Login  </h1>
+      {/* Normally we are useing forms */}
+      <Form onSubmit={sumbitHandler}>
+        {/* for wraping our content into a group of items like Div Started */}
+        <Form.Group className="my-2" controlId="email">
+          {/* Normal like a label input box top Nameing Email Address */}
+          <Form.Label>Email Address</Form.Label>
+          {/* Controll is LIke a input box same as a HTMl */}
+          <Form.Control
+            type="email"
+            placeholder="Enter Email"
+            value={email}
+            onChange={(e)=>{setEmail(e.target.value)}}
+            >
+          </Form.Control>
+        {/* for wraping our content into a group Ended */}
+        </Form.Group>
+       
+        {/* for wraping our content into a group of items like Div Started */}
+        <Form.Group className="my-2" controlId="password">
+          {/* Normal like a label input box top Nameing Email Address */}
+          <Form.Label>Email Password</Form.Label>
+          {/* Controll is LIke a input box same as a HTMl */}
+          <Form.Control
+            type="password"
+            placeholder="Enter Password"
+            value={password}
+            onChange={(e)=>{setPassword(e.target.value)}}
+            >
+          </Form.Control>
+        {/* for wraping our content into a group Ended */}
+        </Form.Group>
 
-            {/* User Email Entering Place and Stored in State SetEmail  */}
-            <Form.Group className="my-2" controlId="email">
-            <Form.Label>Email Address</Form.Label>
-                <Form.Control
-                type="email"
-                placeholder="Enter Email"
-                value={email}
-                onChange={(e)=>setEmail(e.target.value)}
-                >
-                </Form.Control>
-            </Form.Group>
-            
-            {/* User Password Entering Place and Stored in State  */}
-            <Form.Group className="my-2" controlId="password">
-            <Form.Label>Email Address</Form.Label>
-                <Form.Control
-                type="password"
-                placeholder="Enter Password"
-                value={password}
-                onChange={(e)=>setPassword(e.target.value)}
-                >
-                </Form.Control>
-            </Form.Group>
-            <Button type="submit" variant="primary" className="mt-3">
-                Sign In
-            </Button>
-           
-            {/* If user Already registered then Directed to register page  */}
-            <Row className="py-3">
-                <Col>
-                New Customer ? <Link to='/register'>Register</Link>
-                </Col>
-            </Row>
-        </Form>
-      </FormContainer>
-    </div>
+        {/* creating a button for submit values */}
+        <Button type="submit" variant="primary" className='mt-3'>Register</Button>
+
+        <Row className="py-3"> 
+          <Col>
+          New Customer ? <Link to='/register'>Register</Link>
+          </Col>
+        </Row>
+
+      </Form>
+    </FormContainer>
   );
 };
 
--- frontend/src/components/Register/Register.jsx
+++ frontend/src/components/Register/Register.jsx
@@ -4,8 +4,11 @@ import { Link } from "react-router-dom";
 import FormContainer from "../Forms/FormContainer";
 
 const Register = () => {
-  const [email, setEmail] = useState("");
+  const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [mobile, setmobile] = useState("");
+  const [email, setEmail] = useState("");
 
   const sumbitHandler = (e) => {
     e.preventDefault();
@@ -13,7 +16,7 @@ const Register = () => {
   };
   return (
     <FormContainer>
-      <h1>sign in </h1>
+      <h1>Register New User  </h1>
       {/* Normally we are useing forms */}
       <Form onSubmit={sumbitHandler}>
         {/* for wraping our content into a group of items like Div Started */}
@@ -23,13 +26,65 @@ const Register = () => {
           {/* Controll is LIke a input box same as a HTMl */}
           <Form.Control
             type="email"
-            placeholder="Enter Email Address"
+            placeholder="Enter Email"
             value={email}
             onChange={(e)=>{setEmail(e.target.value)}}
             >
           </Form.Control>
         {/* for wraping our content into a group Ended */}
         </Form.Group>
+        {/* for wraping our content into a group of items like Div Started */}
+        <Form.Group className="my-2" controlId="email">
+          {/* Normal like a label input box top Nameing Email Address */}
+          <Form.Label>Email Address</Form.Label>
+          {/* Controll is LIke a input box same as a HTMl */}
+          <Form.Control
+            type="email"
+            placeholder="Enter Email"
+            value={email}
+            onChange={(e)=>{setEmail(e.target.value)}}
+            >
+          </Form.Control>
+        {/* for wraping our content into a group Ended */}
+        </Form.Group>
+        {/* for wraping our content into a group of items like Div Started */}
+        <Form.Group className="my-2" controlId="email">
+          {/* Normal like a label input box top Nameing Email Address */}
+          <Form.Label>Email Address</Form.Label>
+          {/* Controll is LIke a input box same as a HTMl */}
+          <Form.Control
+            type="email"
+            placeholder="Enter Email"
+            value={email}
+            onChange={(e)=>{setEmail(e.target.value)}}
+            >
+          </Form.Control>
+        {/* for wraping our content into a group Ended */}
+        </Form.Group>
+       
+        {/* for wraping our content into a group of items like Div Started */}
+        <Form.Group className="my-2" controlId="password">
+          {/* Normal like a label input box top Nameing Email Address */}
+          <Form.Label>Email Password</Form.Label>
+          {/* Controll is LIke a input box same as a HTMl */}
+          <Form.Control
+            type="password"
+            placeholder="Enter Password"
+            value={password}
+            onChange={(e)=>{setPassword(e.target.value)}}
+            >
+          </Form.Control>
+        {/* for wraping our content into a group Ended */}
+        </Form.Group>
+
+        {/* creating a button for submit values */}
+        <Button type="submit" variant="primary" className='mt-3'>Register</Button>
+
+        <Row className="py-3"> 
+          <Col>
+          Already Customer ? <Link to='/login'>Login</Link>
+          </Col>
+        </Row>
 
       </Form>
     </FormContainer>
