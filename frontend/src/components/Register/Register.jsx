251b057ab5ec61b1eaa76e5f72cbf71416aa8a79
username <your-email@example.com>
2023-09-28 14:19:42 +0530

    trying Tailwind css

--- frontend/src/components/Register/Register.jsx
+++ frontend/src/components/Register/Register.jsx
@@ -1,8 +1,32 @@
-import { Button, Form, Row, Col } from "react-bootstrap";
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import FormContainer from "../Forms/FormContainer";
+// import Container from "react-bootstrap/Container";
+// import Row from "react-bootstrap/Row";
+// import Col from "react-bootstrap/Col";
+// import './Register.css'
+// import { Form } from "react-bootstrap";
 
+// const Register = () => {
+//   return (
+//     <div>
+//       <Container className='mainContainer' fluid={true}>
+//         <Row className="p-3 registrationContainer">
+//           <Col className="left_setion">
+//             <Form>
 
+//             </Form>
+//           </Col>
+//           <Col className="right_setion">2 of 2</Col>
+//         </Row>
 
-export default Register;
+//         <Row>
+//           <Col>1 of 3</Col>
+//           <Col>2 of 3</Col>
+//           <Col>3 of 3</Col>
+//         </Row>
+//       </Container>
+     
+//     </div>
+//   );
+// };
+
+// export default Register;
+
