5603b1664e8bdd70f542f3bc6e5bd3b15584c1dc
username <your-email@example.com>
2023-09-29 13:38:23 +0530

    user api slice created moving to user login page

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,15 @@
-import {configureStore} from '@reduxjs/toolkit'
-import authReducer from './slices/authSlice';
-
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./slices/authSlice";
+import { apiSlice } from "./slices/apiSlice";
 
 const store = configureStore({
-    reducer:{
-        auth:authReducer
-    },
-    middleware:(getDefaultMiddleware)=>getDefaultMiddleware(),
-    devTools:true
+  reducer: {
+    auth: authReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true,
 });
 
 export default store;
