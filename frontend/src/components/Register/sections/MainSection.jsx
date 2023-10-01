7fe30c5004c5f70f7c78a7aae79f9e57d1fedb44
username <your-email@example.com>
2023-10-01 17:23:51 +0530

    some error orrur in login page

--- frontend/src/components/Register/sections/MainSection.jsx
+++ frontend/src/components/Register/sections/MainSection.jsx
@@ -1,33 +1,41 @@
-import backgroundImage from './bg.png'
-import { Container,Row,Col } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 const MainSection = () => {
-    // const styleSteet = {
-    //     width:'93%',
-    //     height:"auto"
-    // }
-    const styleSteet = {
-        color:'white',
-        background:`url(${backgroundImage})`,
-        display:'flex',
-        flexDirection:'column',
-        alignItems:'center',
-        justifyContent:'center',
-        height:'100%',
-        width:'93%',
-        height:"auto"
-
-    }
+  const pContainer = {
+    width: "500px",
+    textTransform: "capitalize",
+    textAlign: "center",
+    height: "auto",
+  };
+  const headerSecondContainer = {
+    width: "500px",
+    textTransform: "capitalize",
+    textAlign: "center",
+    height: "auto",
+  };
+  const styleSteet = {
+    color: "gray",
+    background:
+      "linear-gradient(to right,rgba(15, 23, 42, 1),rgba(23, 45, 100, 1))",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100%",
+    width: "93%",
+  };
   return (
     <>
       <Container>
         <Row className="justify-content-md-center mt-5">
           <Col xs={12} md={6} className="card p-5" style={styleSteet}>
             {/* <Col className="" style={centerContainer}> */}
-            <h1>dd</h1>
-            <h1>dd</h1>
-            <button>hei</button>
-            </Col>
+            <p style={pContainer}>are you struggling for quarters?</p>
+            <h1 style={headerSecondContainer}>
+              be a part of the <br /> Istay
+            </h1>
+           
+          </Col>
           {/* </Col> */}
         </Row>
       </Container>
