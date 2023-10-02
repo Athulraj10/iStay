928bb4a4e676431db8792dd23d09e33fd9b577c1
username <your-email@example.com>
2023-10-02 17:04:04 +0530

    createing logout function

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -3,16 +3,21 @@ import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
 import "./Header.css";
 import logoImage from "./iStays.png";
 import { LinkContainer } from "react-router-bootstrap";
+import { useEffect } from "react";
 
 const Header = () => {
+  // const userInfo = localStorage.getItem("userInfo");
+  const userInfo = localStorage.getItem("userInfo");
+  if (userInfo) {
+    useEffect(() => {
+      if (userInfo) {
+        console.log(userInfo)
+      }
+    });
+  }
   return (
     <header>
-      <Navbar
-        className="navbarStyle"
-        variant="dark"
-        expand="lg"
-        collapseOnSelect
-      >
+      <Navbar className="navbarStyle" variant="dark" expand="lg" collapseOnSelect>
         <Container>
           <LinkContainer to="/">
             <Navbar.Brand>
@@ -23,21 +28,27 @@ const Header = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              
-              {/* login link created for link to login page */}
-              <LinkContainer to='/login' >
-                 <Nav.Link>
-                    <FaSignInAlt /> Login In
-                 </Nav.Link>
-              </LinkContainer>
-
-              {/* login link created for link to register page */}
-              <LinkContainer to='/register'>
-              <Nav.Link>
-                <FaSignOutAlt /> Sign Up
-              </Nav.Link>
-              </LinkContainer>
-           
+              {/* Conditionally render Login/Logout button */}
+              {userInfo ? (
+                // If user information is available, show Logout button
+                <Nav.Link onClick={handleLogout}>
+                  <FaSignOutAlt /> Logout
+                </Nav.Link>
+              ) : (
+                // If user information is not available, show Login and Signup links
+                <>
+                  <LinkContainer to="/login">
+                    <Nav.Link>
+                      <FaSignInAlt /> Login In
+                    </Nav.Link>
+                  </LinkContainer>
+                  <LinkContainer to="/register">
+                    <Nav.Link>
+                      <FaSignOutAlt /> Sign Up
+                    </Nav.Link>
+                  </LinkContainer>
+                </>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
