b252b767897eb592d4080cb7746ec7dc31ba5815
username <your-email@example.com>
2023-10-06 20:31:22 +0530

    text align center

--- frontend/src/components/Admin/Dashboard/AddHostel/AddHostelAdmin.jsx
+++ frontend/src/components/Admin/Dashboard/AddHostel/AddHostelAdmin.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 const AddHostelAdmin = () => {
   return (
-    <div>AddHostelAdmin</div>
-  )
-}
+    <div>
+      <Container>
+        <Row className="photoAddContainer">
+          <Col
+            lg={9}
+            style={{
+              height: "300px",
+              backgroundColor: "gray",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <h5 style={{ textAlign: "center" }}>Primary Photo</h5>
+          </Col>
+          <Col
+            lg={3}
+            style={{ height: "300px", backgroundColor: "green" }}
+          ></Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
-export default AddHostelAdmin
+export default AddHostelAdmin;
