1b080fe3924ee619fcced9ac0402472cb3365aad
username <athulraj165@gmail.com>
2023-10-08 21:34:10 +0530

    a.jsx all file deleted

--- frontend/src/components/Admin/Dashboard/AddHostel/a.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useState } from "react";
-import "./AddHostelAdmin.css";
-import { Container, Form, Row, Col, Button } from "react-bootstrap";
-import { USERSAPI } from "../../../AxiosAPI/AxiosInstance";
-
-const AddHostelAdmin = () => {
-
-}
