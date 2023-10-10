2d1db35c5678fcc87ad304d9a06c92ce60ca72e1
username <athulraj165@gmail.com>
2023-10-10 15:02:51 +0530

    chart section completed

--- frontend/src/components/Sellers/Dashboard/Dashboard.jsx
+++ frontend/src/components/Sellers/Dashboard/Dashboard.jsx
@@ -2,9 +2,11 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { Link } from "react-router-dom";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import { toast } from "react-toastify";
 import { USERSAPI } from "../../AxiosAPI/AxiosInstance";
+import Chart from "chart.js/auto";
+import React from "react";
 
 function Dashboard() {
   const [totalUsers, settotalUsers] = useState([]);
@@ -15,10 +17,10 @@ function Dashboard() {
     const fetchData = async () => {
       try {
         const res = await USERSAPI.post("admin/usersCount");
-        const {userCount,sellerCount,hostelCount} = res.data // Access the data property
-        setTotalSeller(sellerCount)
-        setTotalHostel(hostelCount)
-        settotalUsers(userCount)
+        const { userCount, sellerCount, hostelCount } = res.data; // Access the data property
+        setTotalSeller(sellerCount);
+        setTotalHostel(hostelCount);
+        settotalUsers(userCount);
         setLoading(false);
       } catch (error) {
         toast.error(error.message);
@@ -26,7 +28,61 @@ function Dashboard() {
       }
     };
     fetchData();
-  },[]);
+  }, []);
+
+  const chartRef = React.createRef();
+
+  // Sample data for the chart
+  const data = {
+    labels: ["Item 1", "Item 2", "Item 2", "Item 2", "Item 3", "Item 4"],
+    datasets: [
+      {
+        label: "Sample Data",
+        data: [12, 19, 3, 5, 3, 5],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  // Chart.js options
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
+  React.useEffect(() => {
+    // Create the chart on component mount
+    const ctx = chartRef.current.getContext("2d");
+    let myChart = new Chart(ctx, {
+      type: "bar",
+      data: data,
+      options: options,
+    });
+
+    // Make sure to destroy the previous chart if it exists
+    return () => {
+      if (myChart) {
+        myChart.destroy();
+      }
+    };
+  }, []);
 
   const style = {
     margin: "10px", // Adjust margin as needed
@@ -39,61 +95,43 @@ function Dashboard() {
     textAlign: "center", // Center text
   };
   return (
-    <Container style={{height:'100vh'}}>
-    <Row className="justify-content-md-center">
-      
-      <Col xs lg="3" style={style}>
-        <Link to="/admin/listUsers" style={{ textDecoration: "none" }}>
-          <h3 className="text-center" style={{color:'white'}}>Total Users</h3>
-          <h4 style={{color:'white'}}>Total count : {totalUsers}</h4>
-          <h4 style={{color:'white'}}>Blocked : 0</h4>
-        </Link>
-      </Col>
-      <Col xs lg="3" style={style}>
-        <Link to="/admin/listSellers" style={{ textDecoration: "none" }}>
-          <h3 className="text-center" style={{color:'white'}}>Total Sellers</h3>
-          <h4 style={{color:'white'}}>Total count : {totalSeller}</h4>
-          <h4 style={{color:'white'}}>Blocked : 0</h4>
-        </Link>
-      </Col>
-      <Col xs lg="3" style={style}>
-        <Link to="/admin/listHostels" style={{ textDecoration: "none" }}>
-          <h3 className="text-center" style={{color:'white'}}>Total Hostels</h3>
-          <h4 style={{color:'white'}}>Total count : {totalHostel}</h4>
-          <h4 style={{color:'white'}}>Blocked : 0</h4>
-        </Link>
-      </Col>
+    <div>
+      <Container style={{ height: "auto" }}>
+        <Row className="justify-content-md-center">
+          <Col xs lg="3" style={style}>
+            <Link to="/admin/listUsers" style={{ textDecoration: "none" }}>
+              <h3 className="text-center" style={{ color: "white" }}>
+                Total Users
+              </h3>
+              <h4 style={{ color: "white" }}>Total count : {totalUsers}</h4>
+              <h4 style={{ color: "white" }}>Blocked : 0</h4>
+            </Link>
+          </Col>
+          <Col xs lg="3" style={style}>
+            <Link to="/admin/listSellers" style={{ textDecoration: "none" }}>
+              <h3 className="text-center" style={{ color: "white" }}>
+                Total Sellers
+              </h3>
+              <h4 style={{ color: "white" }}>Total count : {totalSeller}</h4>
+              <h4 style={{ color: "white" }}>Blocked : 0</h4>
+            </Link>
+          </Col>
+          <Col xs lg="3" style={style}>
+            <Link to="/admin/listHostels" style={{ textDecoration: "none" }}>
+              <h3 className="text-center" style={{ color: "white" }}>
+                Total Hostels
+              </h3>
+              <h4 style={{ color: "white" }}>Total count : {totalHostel}</h4>
+              <h4 style={{ color: "white" }}>Blocked : 0</h4>
+            </Link>
+          </Col>
+        </Row>
+        <div style={{ display: 'flex', justifyContent: 'right', alignItems: 'right',marginLeft:'150px', height: '500px', width: '1000px' }}>
+  <canvas ref={chartRef}></canvas>
+</div>
 
-    </Row>
-    <Row>
-      <Col style={style}>additonal details</Col>
-      <Col style={style} md="auto">
-        Variable width content
-      </Col>
-      <Col style={style} xs lg="2">
-        3rd values
-      </Col>
-    </Row>
-  </Container>
-);
+      </Container>
+    </div>
+  );
 }
-
-//     <Container fluid>
-//        <Row style={{height:'200px',backgroundColor:'#233d57'}}>
-//         <Col className='p-3 m-3 text-white' style={{border:'3px solid white'}}xs><h3>Chart</h3></Col>
-//         <Col className='p-3 m-3 text-white' style={{border:'3px solid white'}}xs><h3>Enquery</h3></Col>
-//         <Col className='p-3 m-3 text-white'  xs={{ order: 1 }}  style={{border:'3px solid white'}} ><h3>Revenue</h3></Col>
-//       </Row>
-//       <Row style={{height:'450px',backgroundColor:'#1b2335'}}>
-//       <Col className='p-3 m-3 text-white' style={{border:'3px solid white'}}  xs><h3>Listing Datas</h3></Col>
-//       </Row>
-//       <Row style={{height:'450px',backgroundColor:'#232a4a'}}>
-//       <Col className='p-3 m-3' style={{border:'3px solid white'}}  xs>Listing data</Col>
-//         <Col className='p-3 m-3'  xs={{ order: 12 }} style={{border:'3px solid white'}}>values</Col>
-//         <Col className='p-3 m-3'  xs={{ order: 1 }}  style={{border:'3px solid white'}} >container</Col>    
-//       </Row>
-//     </Container>
-//   );
-// }
-
 export default Dashboard;
