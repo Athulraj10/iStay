330ed3c03f821e429dc0594d0a817d68a5dc5e1a
username <athulraj165@gmail.com>
2023-10-16 14:37:11 +0530

    data received in hostel data usestate

--- frontend/src/components/Users/FindAccommadation/BookingComformation.jsx
+++ frontend/src/components/Users/FindAccommadation/BookingComformation.jsx
@@ -1,4 +1,4 @@
-import React,{useEffect} from 'react'
+import React,{useEffect, useState} from 'react'
 
 import { useLocation } from 'react-router-dom';
 import { USERSAPI } from '../../AxiosAPI/AxiosInstance';
@@ -7,6 +7,7 @@ import { toast } from 'react-toastify';
 
 const BookingComformation = () => {
   const location = useLocation();
+  const [hostelData, setHostelData] = useState(null); // Define the state variable
   const searchParams = new URLSearchParams(location.search);
   
   // Use the get method to retrieve the values
@@ -14,19 +15,33 @@ const BookingComformation = () => {
   const hostelId = searchParams.get('hostel');
 
   useEffect(() => {
+    if (hostelData !== null) {
+      console.log('hostelData has been updated:', hostelData);
+    }
+  }, [hostelData]);
+  
+  useEffect(() => {
     const fetchData = async () => {
       const verificationLocalStorage = localStorage.getItem('bookingStarted');
       if (userId && hostelId && verificationLocalStorage === userId) {
         try {
-          localStorage.removeItem("bookingStarted");
+          localStorage.removeItem('bookingStarted');
           const response = await USERSAPI.get('/users/bookingConfirmation', {
             params: { userId, hostelId },
-          });          
+          });
+
+          if (response.data.bookingCompleted) {
+            const responseData = response.data.hostelData;
+            setHostelData(responseData);
+            toast.success('Booking Completed');
+          } else {
+            toast.error('Booking Not Completed');
+          }
         } catch (error) {
-          toast.error(error)
+          toast.error(error);
         }
-      }else{
-        toast.error("Booking AlreadyCompleted")
+      } else {
+        toast.error('Booking Already Completed');
       }
     };
 
@@ -35,8 +50,7 @@ const BookingComformation = () => {
 
   return (
    <>
-      <h1>`${userId}`</h1>
-      <h1>`${hostelId}`</h1>
+      
    </>
   )
 }
