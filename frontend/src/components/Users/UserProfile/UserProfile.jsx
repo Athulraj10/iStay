77fa674abc93a4b914ba7f7025793110661a0a67
username <athulraj165@gmail.com>
2023-10-20 09:49:30 +0530

    trailing method

--- frontend/src/components/Users/UserProfile/UserProfile.jsx
+++ frontend/src/components/Users/UserProfile/UserProfile.jsx
@@ -18,6 +18,7 @@ export default function UserProfile() {
         if (response.data.userDetails) {
           setUserData(response.data.userDetails)
         }
+        
       } catch (error) {
         if (
           error.response &&
@@ -31,7 +32,7 @@ export default function UserProfile() {
               navigate(`${error.response.data.redirect}`);
             }, 3000);
           }
-          
+
         } else {
           toast.error("Please Login");
           setTimeout(() => {
