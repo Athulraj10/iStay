cdbaeb21d69d6c4146e9668da2b74c0c96f34b04
username <athulraj165@gmail.com>
2023-10-21 19:10:01 +0530

    makeing some changed in admin header

--- frontend/src/components/Admin/AdminHeader/AdminHeader.jsx
+++ frontend/src/components/Admin/AdminHeader/AdminHeader.jsx
@@ -13,15 +13,15 @@ import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { USERSAPI } from "../../AxiosAPI/AxiosInstance";
 
-
 const AdminHeader = () => {
   const location = useNavigate();
   const [adminInfo, setadminInfo] = useState(null);
-
-  const handleLogout =async () => {
-    let res = await USERSAPI.post('/admin/logout')
-    console.log(res)
-    if(res.status){
+  const [adminInfoLoaded, setadminInfoLoaded] = useState(false);
+  
+  const handleLogout = async () => {
+    let res = await USERSAPI.post("/admin/logout");
+    console.log(res);
+    if (res.status) {
       localStorage.removeItem("adminInfo");
       setadminInfo(null);
       location("/admin");
@@ -29,18 +29,22 @@ const AdminHeader = () => {
   };
 
   useEffect(() => {
-    // Define an asynchronous function to fetch adminInfo from localStorage
-    const fetchadminInfo = async () => {
-      const storedadminInfo = localStorage.getItem("adminInfo");
-      if (storedadminInfo) {
-        // You can add await here if needed
-        setadminInfo(storedadminInfo);
+    const fetchAdminInfo = async () => {
+      const storedAdminInfo = localStorage.getItem("adminInfo");
+      if (storedAdminInfo) {
+        setadminInfo(storedAdminInfo);
+        setadminInfoLoaded(true);
       }
     };
+    fetchAdminInfo();
+  }, []); 
 
-    // Call the asynchronous function
-    fetchadminInfo();
-  }, []); // Empty dependency array to run once on mount
+  useEffect(() => {
+    // This effect runs whenever adminInfoLoaded changes
+    if (adminInfoLoaded) {
+      // Add code here to handle changes when adminInfoLoaded is true
+    }
+  }, [adminInfoLoaded]);
 
   return (
     <>
@@ -60,34 +64,29 @@ const AdminHeader = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              
-              <Link to="/admin/listUsers" className="nav-link">
-                <FaUser /> List User
-              </Link>
-
-              <Link to="/admin/listSellers" className="nav-link">
-                <FaStore /> List Seller
-              </Link>
-
-              <Link to="/admin/listHostels" className="nav-link">
-                <FaHome /> List Hostel
-              </Link>
               {/* Conditionally render Login/Logout button */}
               {adminInfo ? (
                 // If user information is available, show Logout button
                 <>
+                  <Link to="/admin/listUsers" className="nav-link">
+                    <FaUser /> List User
+                  </Link>
+
+                  <Link to="/admin/listSellers" className="nav-link">
+                    <FaStore /> List Seller
+                  </Link>
+
+                  <Link to="/admin/listHostels" className="nav-link">
+                    <FaHome /> List Hostel
+                  </Link>
+
                   <Nav.Link onClick={handleLogout}>
                     <FaSignOutAlt /> Logout
                   </Nav.Link>
                 </>
               ) : (
                 <>
-                  <LinkContainer to="/admin/login">
-                    <Nav.Link>
-                      <FaSignInAlt />
-                      &nbsp;
-                    </Nav.Link>
-                  </LinkContainer>
+                 
                 </>
               )}
             </Nav>
