83a8a3098d5b8a8636b986e97b863c89064bc2e3
username <athulraj165@gmail.com>
2023-10-23 21:11:25 +0530

    user wallet aggrigate function

--- backend/controllers/UserControllers/userControllers.js
+++ backend/controllers/UserControllers/userControllers.js
@@ -609,6 +609,7 @@ const addReview = asyncHandler(async (req, res) => {
     console.error(error);
   }
 });
+// ---------------------------Aggregate with wallet---------------------------
 const userProfileWithWalletAggregate = asyncHandler(
   asyncHandler(async (userId) => {
     try {
