76dd8eb8837330c3e8706ddc648fe067abf40a4f
username <athulraj165@gmail.com>
2023-10-24 21:20:14 +0530

    group chat try to implementing

--- backend/controllers/ChatController/chatController.js
+++ backend/controllers/ChatController/chatController.js
@@ -115,13 +115,42 @@ const accessChat = asyncHandler(async (req, res) => {
 
 const fetchChats = asyncHandler(async (req, res) => {
   try {
-    Chat.find({ users: { $elemMatch: { $eq: req.user._id } } }).populate('users','-password').populate('latestMessage').sort({updatedAt:-1}).then(async(results)=>{
-      results = await User.populate(results,{
-        path:'latestMessage.sender',
-        select:'name pic email'
-      })
-      res.status(200).send(results)
-    })
+    Chat.find({ users: { $elemMatch: { $eq: req.user._id } } })
+      .populate("users", "-password")
+      .populate("latestMessage")
+      .sort({ updatedAt: -1 })
+      .then(async (results) => {
+        results = await User.populate(results, {
+          path: "latestMessage.sender",
+          select: "name pic email",
+        });
+        res.status(200).send(results);
+      });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+const createGroupChat = asyncHandler(async (req, res) => {
+  if (!req.body.users || !req.body.name) {
+    return res.status(400).send({ message: "please fill all the fields" });
+  }
+  var users = JSON.parse(req.body.users);
+  if (users.length < 2) {
+    return res.status(400).send("Morethen 2 person needed for a group chat");
+  }
+  users.push(req.user);
+  try {
+    const groupChat = await Chat.create({
+      chatName: req.body.name,
+      users: users,
+      isGroupChat: true,
+      groupAdmin: req.user,
+    });
+    const fullGroupChat = await Chat.findOne({ _id: groupChat._id })
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password");
+    res.status(200).json(fullGroupChat)
   } catch (error) {
     console.log(error);
   }
@@ -134,5 +163,5 @@ export {
   //   -----------------Start chat creation
   accessChat,
   // ---------------------excluding Self Chat
-  fetchChats
+  fetchChats,
 };
