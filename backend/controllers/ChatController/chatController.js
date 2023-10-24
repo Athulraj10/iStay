f218ea11b334791547ca016330a48ed56aceda6d
username <athulraj165@gmail.com>
2023-10-24 20:45:06 +0530

    new its working properly

--- backend/controllers/ChatController/chatController.js
+++ backend/controllers/ChatController/chatController.js
@@ -77,11 +77,11 @@ const accessChat = asyncHandler(async (req, res) => {
       return res.sendStatus(400);
     }
 
-    var isChat = await Chat.findOne({
+    var isChat = await Chat.find({
       isGroupChat: false,
       $and: [
-        { users: { $elemMatch: { $eq:req.user._id } } },
-        { users: { $elemMatch: { $eq:userId } } },
+        { users: { $elemMatch: { $eq: req.user._id } } },
+        { users: { $elemMatch: { $eq: userId } } },
       ],
     })
       .populate("users", "-password")
---
+++ backend/controllers/ChatController/g.js
@@ -0,0 +1,17 @@
+var isChat = await Chat.find({
+    isGroupChat: false,
+    $and: [
+      { users: { $elemMatch: { $eq: req.user._id } } },
+      { users: { $elemMatch: { $eq: userId } } },
+    ],
+  })
+    .populate("users", "-password")
+    .populate("latestMessage");
+
+  isChat = await User.populate(isChat, {
+    path: "latestMessage.sender",
+    select: "name pic email",
+  });
+
+  if (isChat.length > 0) {
+    res.send(isChat[0]);
