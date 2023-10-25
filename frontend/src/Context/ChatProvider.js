b4567f9bb45d7acad13cd2b3354cc761820ea3f6
username <athulraj165@gmail.com>
2023-10-25 09:20:41 +0530

    ChatProvider completed

--- frontend/src/Context/ChatProvider.js
+++ frontend/src/Context/ChatProvider.js
@@ -1,10 +1,25 @@
-import { createContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const ChatContext = createContext()
-const ChatProvider = ({children})=>{
-    return (
-        <ChatContext.Provider>{children}</ChatContext.Provider>
-    )
-}
+const ChatContext = createContext();
+const ChatProvider = ({ children }) => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState();
+  useEffect(() => {
+    const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+    setUser(userInfo);
 
+    if (!userInfo) {
+      navigate("/users/login");
+    }
+  }, [navigate]);
+  return (
+    <ChatContext.Provider value={{ user, setUser }}>
+      {children}
+    </ChatContext.Provider>
+  );
+};
+export const ChatState = () => {
+  return useContext(ChatContext);
+};
 export default ChatProvider;
