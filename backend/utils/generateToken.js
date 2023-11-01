bbdedbe8799386dda86bc84af757f68998ffe2c8
username <athulraj165@gmail.com>
2023-11-01 17:33:08 +0530

    htttp only false

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -6,7 +6,7 @@ const generateToken = (res, userId) => {
     });
 
     res.cookie("jwt_User", token, {
-      httpOnly: true,
+      httpOnly: false,
       secure: process.env.NODE_ENV !== "development",
       sameSite: "strict",
       maxAge: 30 * 24 * 60 * 60 * 1000,
