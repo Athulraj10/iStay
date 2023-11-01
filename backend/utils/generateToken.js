bdb905376fe73400e2933da5cb3dd66f401a5562
username <athulraj165@gmail.com>
2023-11-01 17:45:05 +0530

    addedd

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,5 +1,7 @@
 import jwt from "jsonwebtoken";
 const generateToken = (res, userId) => {
+  console.log(res)
+  console.log(userId)
   try {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
       expiresIn: "30d",
