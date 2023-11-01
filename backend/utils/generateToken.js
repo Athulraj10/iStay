deaa4ef5d99a5b2a3679ab7bed99e7a60bb925a1
username <athulraj165@gmail.com>
2023-11-01 18:31:44 +0530

    token

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,18 +1,15 @@
 import jwt from "jsonwebtoken";
+
 const generateToken = (res, userId) => {
   try {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
       expiresIn: "30d",
     });
-    res.cookie("jwt_User",token,{
-      httpOnly: false,
-      // secure: process.env.NODE_ENV !== "development",
+    res.cookie("jwt_User", token, {
+      httpOnly: true,
       sameSite: "strict",
       maxAge: 30 * 24 * 60 * 60 * 1000,
     });
-    console.log(res.cookie)
-    console.log('token is ' + token)
-    
   } catch (error) {
     console.log(error);
   }
