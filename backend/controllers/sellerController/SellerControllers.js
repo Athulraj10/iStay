304d4cf92e2653bbb2d9760ee2e9397b64b8a8ed
username <athulraj165@gmail.com>
2023-11-05 13:58:01 +0530

    adding required modules

--- backend/controllers/sellerController/SellerControllers.js
+++ backend/controllers/sellerController/SellerControllers.js
@@ -4,3 +4,17 @@ import mongoose from "mongoose";
 import nodemailer from "nodemailer";
 import jwt from "jsonwebtoken";
 
+
+// importing models
+import Seller from "../../models/SellerModel/SellerModel.js";
+import OTP from "../../models/OTPModel.js";
+import Hostel from "../../models/SellerModel/HostelModel.js";
+import Booking from "../../models/BookHostelModel/BookHostelModel.js";
+import Enquiry from "../../models/UserModels/enquery.js";
+import RoomChat from "../../models/chatRoom.js";
+// importing JWT token sellerside
+import generateTokenSeller from "../../utils/generateTokenSeller.js";
+import constants from "../Constants/constants.js";
+import { sellerAggregateRevenue, sellerRevenueAmount, sellerTotal } from "./SellerRevenue.js";
+
+
