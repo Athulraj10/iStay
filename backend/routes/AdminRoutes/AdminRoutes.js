51931202e5440257b1eceea6f6d727478947db77
username <athulraj165@gmail.com>
2023-11-07 20:16:26 +0530

    authentication router

--- backend/routes/AdminRoutes/AdminRoutes.js
+++ backend/routes/AdminRoutes/AdminRoutes.js
@@ -36,42 +36,4 @@ import {
 import { adminMiddleware } from "../../middleware/AdminMiddleware/authMiddleware.js";
 const AdminRoute = express.Router();
 
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-      cb(null, 'backend/public/images');
-  },
-  filename: (req, file, cb) => {
-    cb(null, `${Date.now()}-${file.originalname}`);
-  },
-});
-
-const upload = multer({ storage });
-
-// -----------------Admin ManageMent
-AdminRoute.post("/login", adminAuthentication);
-AdminRoute.post("/forget", adminForget);
-AdminRoute.post("/verifyOTP", adminVerifyOTP);
-AdminRoute.post("/resetPassword", adminResetPassword);
-
-// -------------User Management
-AdminRoute.get("/listUser", listUser);
-AdminRoute.patch("/listUser/block/:id", blockUser);
-
-// ----------------Seller Management
-AdminRoute.post("/listSellers", listSellers);
-AdminRoute.patch("/listSeller/block/:id",blockSeller);
-
-// --------------HostelManagement
-AdminRoute.post('/listHostels',listHostelsAdmin);
-AdminRoute.patch('/listHostel/block/:id',BlockHostelsAdmin);
-
-
-//-------------------- Dashboard Values
-AdminRoute.get("/dashboard",dashboardValuesCount);
-
-// // AdminRoute.post('/',)
-// AdminRoute.route('/profile').get(protect,getUserProfile).put(protect,updateUserProfile)
-AdminRoute.post('/logout',logoutAdmin)
-
 export default AdminRoute;
