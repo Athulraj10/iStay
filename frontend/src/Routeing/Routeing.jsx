e23e056ec8fbbf740167be9141b71685483a0e63
username <athulraj165@gmail.com>
2023-11-09 21:31:45 +0530

    not foundscreen imported

--- frontend/src/Routeing/Routeing.jsx
+++ frontend/src/Routeing/Routeing.jsx
@@ -1,2 +1,5 @@
 import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
+
+// ------------Error Page Global
+import NotFoundScreen from "../components/ErrorPage/ErrorPage";
