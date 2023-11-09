06849f69c32a2c80186692646575a757ff748b1e
username <athulraj165@gmail.com>
2023-11-09 21:32:03 +0530

    user side panel files

--- frontend/src/Routeing/Routeing.jsx
+++ frontend/src/Routeing/Routeing.jsx
@@ -3,3 +3,6 @@ import { Route, Routes } from "react-router-dom";
 
 // ------------Error Page Global
 import NotFoundScreen from "../components/ErrorPage/ErrorPage";
+
+
+// --------------User Panel-----------
