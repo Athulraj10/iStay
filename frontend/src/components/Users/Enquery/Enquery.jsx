b446c0f1fb1c1d5d66920d3caf71c21a4751cc6d
username <athulraj165@gmail.com>
2023-11-11 17:40:25 +0530

    user qnquery on process

--- frontend/src/components/Users/Enquery/Enquery.jsx
+++ frontend/src/components/Users/Enquery/Enquery.jsx
@@ -14,6 +14,7 @@ const Enquery = () => {
       try {
         let response = await USERSAPI.get("/users/listenqueryreplyuser");
         if (response.data.enquery) {
+          console.log(response.data.userEnquiry)
           setEnquery(response.data.userEnquiry);
         }
       } catch (error) {
